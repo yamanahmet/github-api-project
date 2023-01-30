bf9c788db76bffc100bbd705c66f70c10289a7c7
Ahmet YAMAN <ymna32@gmail.com>
2023-01-30 21:38:27 +0300

    clean code

--- src/main/java/com/hitit/controllers/ContributorController.java
+++ src/main/java/com/hitit/controllers/ContributorController.java
@@ -17,62 +17,11 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class ContributorController {
-
-    RestTemplate restTemplate;
-
     final ContributorService contributorService;
-
 
     @GetMapping(value = "/contributors", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     @ResponseStatus(HttpStatus.ACCEPTED)
     public ResponseEntity<List<List<Contributors>>> findAllContributors() {
         return ResponseEntity.ok(contributorService.findContributors());
     }
-
-//    @GetMapping(value = "/ccc")
-//    public String getContributors(){
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-//        HttpEntity<String> entity = new HttpEntity<String>(headers);
-//
-//        return restTemplate.exchange("" +
-//                "https://api.github.com/users/yamanhamet", HttpMethod.GET, entity, String.class).getBody();
-//    }
-
-//    @Autowired
-//    WebClientService webClientService;
-//
-
-
-//    @GetMapping("/user/{id}")
-//    @ResponseStatus(HttpStatus.ACCEPTED)
-//    public Mono<Users> getUserById(@PathVariable Long id){
-//        return webClientService.findUserById(id);
-//    }
-//
-//    @GetMapping(value = "/users", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-//    @ResponseStatus(HttpStatus.ACCEPTED)
-//    public Flux<Users> findAllUsers(){
-//        return webClientService.findUsers();
-//    }
-
-
-//    @PostMapping("/save")
-//    @ResponseStatus(HttpStatus.CREATED)
-//    public void saveUser(@RequestBody Users users){
-//        webClientService.saveUser(users);
-//    }
-//
-//    @PutMapping("/update")
-//    public Mono<Users> updateUser(@RequestBody Users user){
-//        return webClientService.updateUser(user);
-//    }
-//
-//    @DeleteMapping("/user/{id}")
-//    @ResponseStatus(HttpStatus.OK)
-//    public Mono<Void> deleteUser(@PathVariable Long id){
-//        return webClientService.deleteUser(id);
-//    }
-
-
 }
--- src/main/java/com/hitit/services/ContributorService.java
+++ src/main/java/com/hitit/services/ContributorService.java
@@ -1,45 +1,28 @@
 package com.hitit.services;
 
-import com.hitit.GithubApiProjectApplication;
 import com.hitit.dto.Contributors;
 import com.hitit.dto.Users;
-import org.apache.catalina.User;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class ContributorService {
 
-    //    private final WebClient webClient;
-    private final String APACHE_URI = "https://api.github.com/repos/apache/"; //https://api.github.com/repos/apache/echarts
-    private final String[] REPO_NAME = {"echarts", "superset", "dubbo", "spark", "airflow"};
+    private final String APACHE_URI = "https://api.github.com/repos/apache/";
     private final String GITHUB_URI = "https://api.github.com/users/";
+    private final String[] REPO_NAME = {"echarts", "superset", "dubbo", "spark", "airflow"};
 
-//    private GithubApiProjectApplication githubApiProjectApplication;
-
-//    public ContributorService(WebClient.Builder webClientBuilder) {
-//        this.webClient = webClientBuilder.baseUrl("https://api.github.com/repos/apache/echarts").build();  //{echarts, superset, dubbo, spark, airflow}
-//    }
-
-    //    public Flux<Contributors> findContributors(){
-//        return this.webClient.get().uri("/contributors")
-//                .retrieve()
-//                .bodyToFlux(Contributors.class);
-//    }
-    public List<List<Contributors>> findContributors(){
+    public List<List<Contributors>> findContributors() {
         RestTemplate restTemplate = new RestTemplate();
         List<List<Contributors>> resultAll = new ArrayList<>();
         for (String repoName : REPO_NAME) {
@@ -56,11 +39,10 @@ public class ContributorService {
                     .map(contributors -> {
                         Users user = findUser(contributors.getLogin());
                         user.setContributions(contributors.getContributions());
-//                    githubApiProjectApplication.write(APACHE_URI, user.getClass().isArray());
                         return user;
                     })
-//                .peek(System.out::println)
                     .collect(Collectors.toList());
+
             BufferedWriter writer = null;
             try {
                 writer = new BufferedWriter(new FileWriter("test.txt", true));
@@ -70,39 +52,15 @@ public class ContributorService {
                     System.out.println(line);
                 }
                 writer.close();
+                System.out.println("Successfully wrote to the file.");
             } catch (IOException e) {
-                throw new RuntimeException(e);
+                System.out.println("An error occurred.");
+                e.printStackTrace();
             }
             resultAll.add(result);
         }
-        try {
-//            if (CollectionUtils.isEmpty(resultAll)) {
-//                FileWriter myWriter = new FileWriter("C:\\Users\\ayaman\\Downloads\\users.txt");
-//                for (List<Contributors> contributors : resultAll) {
-//                     myWriter.append(contributors.toString());
-//                }
-//                myWriter.close();
-////                return
-//                // Error resultAll
-//            }
-// write txt
-            // OK
-            return resultAll;
-        } catch (Exception e) {
-// ERROR
-            return resultAll;
-        }
+        return resultAll;
     }
-
-//    private String login; //username
-//    private String location;
-//    private String company;
-//    private int contributions;
-
-//    https://api.github.com/users/pissang
-
-
-    // repo:commons-lang ,user:garydgregory , location: Denver, CO, USA, company: Rocket Software, contributions: 845
 
     private Users findUser(String username) {
         RestTemplate restTemplate = new RestTemplate();
@@ -112,10 +70,6 @@ public class ContributorService {
                 });
 
         Users result = response.getBody();
-
-//        githubApiProjectApplication.write(APACHE_URI, (List<Users>) findUser("pissang"));
         return result;
     }
-
-
 }
